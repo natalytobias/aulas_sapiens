e7bcec3993ac38b34369911356c8d9ba6c006c49
Nataly Tobias <nataly.tobias11@gmail.com>
2021-03-22 17:07:51 -0300

    first commit - teste

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+     echo("Atividade de Fixação <br/> Nome - Nataly
+          <br/> Sobrenome - Tobias
+          <br/> número da casa - 1388
+          <br/> bairo - são josé
+          <br/> cidade - guajará-mirim
+          <br/> estado - Rondonia
+          <br/> email - nataly.tobias11@gamil.com
+          <br/> telefone - (69) 8433-3879
+          <br/> nome dos pais - rosely e ademir
+          <br/> data de nascimento - 21/03/2002
+          <br/> data atual - 16/03/2020
+          <br/> estado civil - solteiro
+          <br/> idade atual - 18 anos
+          <br/> cadastro ativo - ");
+
+          
+?>
+
